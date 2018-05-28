466a6857903a62a907bd6233a315f21413798ca0
Antoine Musso <hashar@free.fr>
2018-05-28 17:25:17 +0200

    Fix database schema for SQLite
    Change-Id: I6268b1385fcf3b9ebbd718d5c47c4602d12a1777

--- closewikis.sql
+++ closewikis.sql
@@ -4,7 +4,6 @@ CREATE TABLE /*$wgDBprefix*/closedwikis (
 	cw_wiki varchar(255) NOT NULL,
 	cw_reason mediumblob NOT NULL,
 	cw_timestamp binary(14) NOT NULL,
-	cw_by tinyblob NOT NULL,
-
-	UNIQUE INDEX cw_wiki (cw_wiki)
+	cw_by tinyblob NOT NULL
 ) /*$wgDBTableOptions*/;
+CREATE UNIQUE INDEX cw_wiki ON closedwikis (cw_wiki);
