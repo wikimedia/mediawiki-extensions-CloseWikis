6c8c053df2db59fe1e476e06c735d144c7e0f043
Umherirrender <umherirrender_de.wp@web.de>
2022-02-05 22:00:07 +0100

    Remove call of deprecated IDatabase::freeResult
    Bug: T286694
    Change-Id: Iba70ac944dcc302ae5641255451e8245f8c39fac

--- includes/CloseWikis.php
+++ includes/CloseWikis.php
@@ -27,7 +27,6 @@ class CloseWikis {
 		foreach ( $result as $row ) {
 			$list[] = new CloseWikisRow( $row );
 		}
-		$dbr->freeResult( $result );
 		return $list;
 	}
 
@@ -45,7 +44,6 @@ class CloseWikis {
 		foreach ( $result as $row ) {
 			$list[] = $row->cw_wiki;
 		}
-		$dbr->freeResult( $result );
 		self::$cachedList = $list;
 		return $list;
 	}
